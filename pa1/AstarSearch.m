934ffa6a7b771f3cd7f257b6f70b96395c875b59
jbenet <johnnyklaj@gmail.com>
2009-10-12 20:48:09 -0700

    A star

--- pa1/AstarSearch.m
+++ pa1/AstarSearch.m
@@ -8,7 +8,57 @@ function [path,cost,nodesExpanded] = AstarSearch(World)
 % two points are not likely to be visible from each other). Delete these
 % lines and implement a full, general search.
 
-[path, cost, nodesExpanded] = uniformCostSearch(World);
+heap = heapCreate();
+
+expanded = [];
+
+first_node.value = 0;
+first_node.sofar = 0;
+first_node.path = [1];
+heap = heapPush(heap, first_node);
+
+nodesExpanded = 0;
+
+while ~heapIsEmpty(heap)
+    [top, heap] = heapPop(heap);
+        
+    last = top.path(length(top.path));
+    if (last == size(World.Landmarks, 2))
+        path = top.path
+        cost = top.sofar;
+        x = 2
+        return;
+    end
+    
+    successors = find(World.Connectivity(:,last));
+    for suc_num = 1:size(successors)
+        suc = successors(suc_num);
+
+        find(expanded == suc, 1);
+        size(find(expanded == suc, 1), 2);
+        
+        if (size(find(expanded == suc, 1), 2) ~= 0)    
+            continue;
+        end
+        
+        expanded = [expanded suc];
+        nodesExpanded = nodesExpanded + 1;
+        
+        World.Landmarks(:,last);
+        World.Landmarks(:,suc);
+        dFrom = sqrt(sum((World.Landmarks(:,last) - World.Landmarks(:,suc)) .^ 2));
+        dTo   = sqrt(sum((World.Landmarks(:,size(World.Landmarks, 2)) - World.Landmarks(:,suc)) .^ 2));
+        new_node.value = top.sofar + dFrom + dTo;
+        new_node.sofar = top.sofar + dFrom;
+        new_node.path = [top.path suc];
+        heap = heapPush(heap, new_node);
+    end
+end
+
+x = 1
+
+path = [];
+cost = inf;
 
 % path = [1 size(World.Landmarks,2)];
 % cost = norm(World.Landmarks(:,1) - World.Landmarks(:,end));
