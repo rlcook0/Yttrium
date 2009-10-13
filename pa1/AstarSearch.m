d4c8d02435e2f78a579ec4663c1bd5db831911b9
jbenet <johnnyklaj@gmail.com>
2009-10-12 20:04:39 -0700

    All searches use Uniform

--- pa1/AstarSearch.m
+++ pa1/AstarSearch.m
@@ -7,7 +7,10 @@ function [path,cost,nodesExpanded] = AstarSearch(World)
 % configuration to the goal, but such a path is likely invalid (as these
 % two points are not likely to be visible from each other). Delete these
 % lines and implement a full, general search.
-path = [1 size(World.Landmarks,2)];
-cost = norm(World.Landmarks(:,1) - World.Landmarks(:,end));
-nodesExpanded = 0;
+
+[path, cost, nodesExpanded] = uniformCostSearch(World);
+
+% path = [1 size(World.Landmarks,2)];
+% cost = norm(World.Landmarks(:,1) - World.Landmarks(:,end));
+% nodesExpanded = 0;
 
