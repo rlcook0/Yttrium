f16b64eb9350faaa9a72a784caf5756b1bcc3999
Ivan Lee <iylee@stanford.edu>
2009-10-12 23:11:50 -0700

    bestFirstSearch now works

--- pa1/bestFirstSearch.m
+++ pa1/bestFirstSearch.m
@@ -8,9 +8,77 @@ function [path,cost,nodesExpanded] = bestFirstSearch(World)
 % two points are not likely to be visible from each other). Delete these
 % lines and implement a full, general search.
 
-[path, cost, nodesExpanded] = AstarSearch(World);
+%[path, cost, nodesExpanded] = AstarSearch(World);
 
-%path = [1 size(World.Landmarks,2)];
-%cost = norm(World.Landmarks(:,1) - World.Landmarks(:,end));
-%nodesExpanded = 0;
+heap = heapCreate();
 
+% List to capture all the nodes we have expanded so far.
+expanded = [];
+nodesExpanded = 0;
+costThusFar = 0;
+
+% Put the first node in the queue/heap.
+first_node.value = 0;
+first_node.path = [1];
+% Needed to keep track of the path's overall cost.
+first_node.distanceFromPrev = 0;
+heap = heapPush(heap, first_node);
+
+while ~heapIsEmpty(heap)
+    [curr, heap] = heapPop(heap);
+    
+    % Grab the last node in the path.
+    curr_index = curr.path(length(curr.path));
+    costThusFar = costThusFar + curr.distanceFromPrev;
+    
+    
+    % Check if the last node in the path is a goal.
+    if (curr_index == size(World.Landmarks, 2))
+        path = curr.path;
+        cost = costThusFar;
+        
+%         fprintf('ANSWER:');
+%         for node_num = 1:length(path)
+%             coord = World.Landmarks(:,path(node_num));
+%             fprintf('%d-(%f, %f) ', curr.path(node_num), coord(1), coord(2));
+%         end
+%         fprintf('\n');
+        
+        return;
+    end
+    
+    % Since we haven't...
+    nodesExpanded = nodesExpanded + 1;
+        
+    % Track this node as expanded
+    expanded = [expanded curr_index];
+    
+    % Loop over all successors of the current node.
+    successors = find(World.Connectivity(:,curr_index));
+    for successor_num = 1:size(successors)
+        successor = successors(successor_num);
+        
+        % Check if we have already expanded this node.
+        if (size(find(expanded == successor, 1), 2) ~= 0)    
+            continue;
+        end
+        
+        % Make a new node.
+        distance = sqrt(sum((World.Landmarks(:,end) - World.Landmarks(:,successor)) .^ 2));
+        new_node.value = distance;
+        new_node.path = [curr.path successor];
+        new_node.distanceFromPrev = sqrt(sum((World.Landmarks(:,curr_index) - World.Landmarks(:,successor)) .^ 2))
+        heap = heapPush(heap, new_node);
+        
+        % TESTING - REMOVE ME
+%         for node_num = 1:length(new_node.path)
+%             coord = World.Landmarks(:,new_node.path(node_num));
+%             fprintf('%d-(%f, %f) ', new_node.path(node_num), coord(1), coord(2));
+%         end
+%         fprintf('\n');
+    end
+end
+
+% e.g. return FAIL
+path = [];
+cost = inf;
