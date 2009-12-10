aa7a1e00347866dc08eeb8387d497178fc870f0a
jbenet <jbenet@cs.stanford.edu>
2009-12-10 12:03:19 -0800

    please, science

--- vision/classifier.cpp
+++ vision/classifier.cpp
@@ -377,8 +377,8 @@ bool Classifier::run(const IplImage *frame, CObjectList *objects, bool scored)
     
     // move old objects
     for (int i = 0; i < (int)objects->size(); ++i) {
-        (*objects)[i].rect.x -= totalXDiff * 10;
-        (*objects)[i].rect.y -= totalYDiff * 10;
+        (*objects)[i].rect.x -= totalXDiff * 5;
+        (*objects)[i].rect.y -= totalYDiff * 5;
     }
     
     cout << "Flow: " << totalXDiff << " " << totalYDiff << endl;
